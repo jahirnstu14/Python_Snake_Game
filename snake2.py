3146a4ca987d7ccd0556626f20690dfd3e6fa65e
jahirul <jahirkhan.ice.nstu@gmail.com>
2023-02-28 00:32:03 +0600

    intial commit

---
+++ snake2.py
@@ -0,0 +1,49 @@
+import pygame 
+pygame.init()
+
+#colors 
+white =(255, 255, 255)
+red =(255, 0, 0)
+black=(0, 0, 0)
+
+screen_height = 500
+screen_weight =300
+#creating window 
+gameWindow=pygame.display.set_mode((screen_height,screen_weight))
+
+pygame.display.set_caption("Snake Game")
+pygame.display.update()
+
+
+#game specific variable
+exit_game=False
+game_over=False
+snake_x=45
+snake_y=55
+snake_size=10
+fps =30
+
+clock=pygame.time.Clock()
+#game loop 
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game==True
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                snake_x=snake_x+10
+
+            if event.key==pygame.K_LEFT:
+                snake_x=snake_x-10
+            if event.key==pygame.K_UP:
+                snake_y=snake_y-10
+            if event.key==pygame.K_DOWN:
+                snake_y=snake_y+10
+
+    gameWindow.fill(white)
+    pygame.draw.rect(gameWindow, black, [snake_x, snake_y,snake_size, snake_size])
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit()
+quit()
